c8f5b60258c96fbf40f65117de0cd9897acd2bf5
MATEUS  REIS   EVANGELISTA <71729952+mre-push@users.noreply.github.com>
2024-04-05 19:02:40 -0300

    Add files via upload

--- Aprendizagem_Maquina_atual/Aprendizagem_Maquina_atual.py
+++ Aprendizagem_Maquina_atual/Aprendizagem_Maquina_atual.py
@@ -7,13 +7,11 @@ from keras.layers import Dense           			# Layer do tipo densamente conectado
 from keras.utils import np_utils         			# Usaremos dela o metodo 'to_categorical()'
 
 # Carregando o Dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = x_train.reshape(60000, 784)              # reshape(linhas, colunas)
-x_test = x_test.reshape(10000, 784)
-
-x_train = x_train/255.0    
-x_test = x_test/255.0	
+#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#x_train = x_train.reshape(60000, 784)              # reshape(linhas, colunas)
+#x_test = x_test.reshape(10000, 784)
+#x_train = x_train/255.0    
+#x_test = x_test/255.0	
 
   # Reshape training data _ RETIRADO DE CONSULTA mateus transformando lista em matriz
 mnist_data = MNIST(os.path.abspath(path))
@@ -22,7 +20,7 @@ X_train = np.reshape(np.asarray(X, dtype=np.uint8), (60000, 28, 28, 1))
 Y_train = np.reshape(np.asarray(Y, dtype=np.uint8), (60000,))
   # Normalize data for better results
 X_train = X_train.astype('float32')/255
-# Convert labels to categoricals (0 -> [1 0 0 0 0 0 0 0 0 0])
+  # Convert labels to categoricals (0 -> [1 0 0 0 0 0 0 0 0 0])
 Y_train = keras.utils.to_categorical(Y_train, num_classes=10)
   # Reshape test data _ RETIRADO DE CONSULTA mateus 
 X, Y = mnist_data.load_testing()
@@ -33,7 +31,7 @@ X_test = X_test.astype('float32')/255
   # Convert labels to categoricals (5 -> [0 0 0 0 0 1 0 0 0 0])
 Y_test = keras.utils.to_categorical(Y_test, num_classes=10)
 def new_func(X_train, Y_train, X_test, Y_test):
- #return (X_train, Y_train), (X_test, Y_test)
+  # return (X_train, Y_train), (X_test, Y_test)
     
 
  #return new_func(X_train, Y_train, X_test, Y_test)
@@ -106,7 +104,7 @@ def remove_marks(ax):
   ax.get_yaxis().set_ticks([])
   ax.get_xaxis().set_ticklabels([])
   ax.get_yaxis().set_ticklabels([])
-# MODELOS de TREINO _ RETIRADO DE CONSULTA mateus
+#MODELOS de TREINO _ RETIRADO DE CONSULTA mateus
 def train_models(models_params, X_train, Y_train, X_test, Y_test):
   data = {}
   for model_params in models_params:
@@ -160,8 +158,126 @@ def load_models(models_params, X_test, Y_test):
     data[name] = (history, dict(zip(metrics_names, metrics)))
   return data
 
+def present_metrics(data):
+  # Crie as figuras e eixos para traçar os dados
+  loss = plt.figure("Loss").gca()
+  accuracy = plt.figure("Accuracy").gca()
+  precision = plt.figure("Precision").gca()
+
+  for name, (history, metrics) in data.items():
+    # Métricas de plotagem
+    loss.plot(history['val_loss'], label=name)
+    accuracy.plot(history['val_acc'], label=name)
+    precision.plot(history['val_precision'], label=name)
+
+    # Imprimir resultados de testes
+    print(name)
+    for name, metric in metrics.items():
+      print("{}: {}".format(name, metric))
+    print()
+
+  # Embeleze os gráficos
+  loss.set_xlabel("Epochs")
+  loss.set_ylabel("Loss (Categorical Crossentropy)")
+  loss.set_title("Loss Evolution")
+  loss.set_ylim(0, 0.15)
+  loss.set_xticks(list(range(20)))
+  loss.legend()
+
+  accuracy.set_xlabel("Epochs")
+  accuracy.set_ylabel("Accuracy")
+  accuracy.set_title("Accuracy Evolution")
+  accuracy.set_ylim(0.95, 1)
+  accuracy.set_xticks(list(range(20)))
+  accuracy.legend()
+
+  precision.set_xlabel("Epochs")
+  precision.set_ylabel("Precision")
+  precision.set_title("Precision Evolution")
+  precision.set_ylim(0.95, 1)
+  precision.set_xticks(list(range(20)))
+  precision.legend()
+  return loss, accuracy, precision
+
+def save_all_figs():
+  for label in plt.get_figlabels():
+    fig = plt.figure(label)
+    fig.savefig('img/'+label+'.png')
+
+def visualize_filters(model):
+  # Carregar filtros para exibição
+  layers = model.get_filters()
+
+  # Mostrar todos os filtros de neurônios da primeira camada em um único gráfico
+  layer = 'first_conv'
+  fig = plt.figure(layer+'-filters-all_neurons')
+  axes = fig.subplots(2,2)
+  axes = list(axes.flatten())
+  filt = layers[layer][0]
+  ax_num = 0
+  for i in range(filt.shape[2]):
+    for o in range(filt.shape[3]):
+      axes[ax_num].matshow(filt[:,:,i:i+1,o:o+1].squeeze(), cmap='gray')
+      axes[ax_num].set_title('{}-{}-{}'.format(layer, i, o))
+      remove_marks(axes[ax_num])
+      ax_num += 1
+
+  # Mostre todos os 4 canais de cada neurônio da segunda camada em um único gráfico
+  layer = 'second_conv'
+  filt = layers[layer][0]
+  for o in range(filt.shape[3]):
+    fig = plt.figure(layer+'-filters-neuron_'+str(o))
+    axes = fig.subplots(2,2)
+    axes = list(axes.flatten())
+    ax_num = 0
+    for i in range(filt.shape[2]):
+      axes[ax_num].matshow(filt[:,:,i:i+1,o:o+1].squeeze(), cmap='gray')
+      axes[ax_num].set_title('{}-{}-{}'.format(layer, i, o))
+      remove_marks(axes[ax_num])
+      ax_num += 1
+
+def visualize_activations(model, X):
+  layers = {
+    'first_conv': ((2,2), {}),
+    'second_conv': ((2,3), {'fontsize':10.5}),
+  }
+  
+  # Apresente as ativações como exemplo
+  for layer, (subplots, title_properties) in layers.items():
+    # Obtenha a ativação para a entrada especificada
+    activation = model.get_activation(X, layer_name=layer)
+    fig = plt.figure(layer+'-neurons_activation')
+    axes = fig.subplots(*subplots)
+    axes = list(axes.flatten())
+    for neuron in range(activation.shape[3]):
+      axes[neuron].matshow(activation[0:1,:,:,neuron:neuron+1].squeeze(), cmap='gray')
+      axes[neuron].set_title('{}-neuron_{}'.format(layer, neuron), **title_properties)
+      remove_marks(axes[neuron])
+
+def visualize_wrong(model, X_test, Y_test, n_samples=5):
+  prediction = np.round(model.predict(X_test))
+  cond = (prediction != Y_test).any(axis=1)
+  X_wrong = X_test[cond]
+  Y_wrong = Y_test[cond]
+  pred_wrong = prediction[cond]
+
+  for i in range(n_samples):
+    X = X_wrong[i:i+1,:,:,:].squeeze()
+    Y = Y_wrong[i:i+1,:].squeeze().argmax()
+    pred = pred_wrong[i:i+1,:].squeeze().argmax()
+
+    fig = plt.figure('wrong_prediction_{}'.format(i))
+    fig.gca().matshow(X, cmap='gray')
+    remove_marks(fig.gca())
+
+    fig.text(0.68,0.75+0.11-0.05,'Esperado:',fontsize=28)
+    fig.text(0.68,0.5+0.11-0.05, str(Y),fontsize=100)
+    fig.text(0.68,0.25+0.11,'Predito:',fontsize=28)
+    fig.text(0.68,0.11, str(pred),fontsize=100)
+    fig.subplots_adjust(left=0.04, right=0.68)
 
 
+# In[ ]:
 
 # Camada Oculta
 model.add(Dense(256, input_dim=784, activation='relu'))       # Adicionando a camada densa. Dense(qtde_de_neurônios, input_dim = qtde_de_entradas, Activation='tipo_de_ativação')
